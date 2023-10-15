9df6cdf61a6030c37349349f9e947580256cd875
Andrei <zoitber9@mail.ru>
2023-10-15 23:53:48 +0300

    fix some

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import './App.css'
+import { FunctionComponent } from 'react';
 
-function App() {
+import './styles/typography.css';
 
+import { useAppSelector, useAppDispatch } from './redux/hooks';
+import { setIsTestStarted, setSentences } from './redux/store/testSlice';
+
+
+import Test from './component/test/test.tsx';
+import ModalWindow from './component/ui/modal/modal.tsx';
+import Button from './component/ui/button/button.tsx';
+
+const App:FunctionComponent = () => {
+  const dispatch = useAppDispatch();
+  const isTestStarted = useAppSelector(state => state.testSlice.isTestStarted);
+
+  const testStateToggler = () => dispatch(setIsTestStarted(true));
 
   return (
-    <>
-    </>
-  )
-}
+      <>
+        <main className='container main'>
+          {
+            isTestStarted
+                ? <Test />
+                : <ModalWindow title='Take a typing test'>
+                  <Button btnText='start' onClick={testStateToggler} />
+                </ModalWindow>
+          }
+        </main>
 
-export default App
+      </>
+  );
+};
+
+export default App;
